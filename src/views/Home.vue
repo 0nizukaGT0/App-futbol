9425d36ae927d53bc780c801fe53272e4412482f
0nizukaGT0 <47951953+0nizukaGT0@users.noreply.github.com>
2019-04-04 11:26:22 +0200

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,8 @@
             Aqui añadiremos un input que hara una mutaion a la fecha actual , eso trigerea el
              getter que nos devuelve fechas cercanas a eso , en este compoente debemos ver que hacer si no hya nada</h4>
             <v-card v-if='isAdmin'>
-              
-              <v-text>Saludos {{currentUser}} bienvenido a la app de futbol 🤖</v-text>
+
+              <v-text>Saludos <b>{{currentUser}}</b> bienvenido a la app de futbol 🤖</v-text>
             </v-card>
             <v-card></v-card>
           </v-flex>
